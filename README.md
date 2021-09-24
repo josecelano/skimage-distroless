e8d771fe68fb7bf60dd7f8dff1092930a3f68b70
Jose Celano <josecelano@gmail.com>
2021-09-24 14:30:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Skimage with docker
+
+Build:
+```
+docker build -t skimage-distroless .
+```
+
+Run:
+```
+docker run -it skimage-distroless
+```
---
+++ main.py
@@ -0,0 +1,10 @@
+from skimage import img_as_ubyte
+from skimage.io import imread, imsave
+from skimage.transform import resize
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
